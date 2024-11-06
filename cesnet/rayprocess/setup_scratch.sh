fc3c85c1174a0ef1ac6460f799aec9d54cb65f1c
Tomea3 <72706095+Tomea3@users.noreply.github.com>
2024-11-06 12:35:58 +0100

    Update setup_scratch.sh

--- cesnet/rayprocess/setup_scratch.sh
+++ cesnet/rayprocess/setup_scratch.sh
@@ -3,8 +3,8 @@
 
 # copy input file and raycloudtools to scratch
 
-cp /storage/brno2/home/tomea/STSM_pokusy/singularity_img/raycloudtools.img $SCRATCHDIR && echo "raycloud copied" >> $LOG_FILE || { echo "raycloud cp error" >> $LOG_FILE; exit 1; }
-cp /storage/brno2/home/tomea/STSM_pokusy/singularity_img/pdal.img $SCRATCHDIR && echo "pdal copied" >> $LOG_FILE || { echo "pdal cp error" >> $LOG_FILE; exit 1; }
+cp /storage/brno2/home/tomea/STSM_pokusy/raycloudtools.img $SCRATCHDIR && echo "raycloud copied" >> $LOG_FILE || { echo "raycloud cp error" >> $LOG_FILE; exit 1; }
+cp /storage/brno2/home/tomea/STSM_pokusy/pdal.img $SCRATCHDIR && echo "pdal copied" >> $LOG_FILE || { echo "pdal cp error" >> $LOG_FILE; exit 1; }
 cp $DATADIR/$SOURCE_DATA  $SCRATCHDIR && echo "data copied" >> $LOG_FILE || { echo "data cp error" >> $LOG_FILE; exit 1; }
 
 
